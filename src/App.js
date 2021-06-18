8fff826fd1c61013b9a7eb9c291800d02756ef3c
ktmihs <yellowgreen423@gmail.com>
2021-06-18 22:36:51 +0900

    Component 최적화

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-// onCreate처럼 새로 생성 시에는 concat이나 spread를 사용
-// onRemove처럼 삭제할 때는 filter를 사용
-// onToggle처럼 업데이트할 때는 map을 사용 
+/**
+ * 컴포넌트 최적화
+ * 
+ * useMemo 연산된 값 재사용
+ * useCallback 특정함수 재사용
+ * React.memo 렌더링 된 결과 재사용
+ */
 
-import React,{useRef, useState} from 'react';
+import React,{useRef, useState, useMemo, useCallback} from 'react';
 import './App.css'
-import Counter from './Counter';
 import CreateUser from './CreateUser';
-import InputSample from './InputSample';
 import UserList from './UserList';
-import UserList2 from './UserList2';
-import UserList3 from './UserList3';
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는 중...')
+  return users.filter(user=>user.active).length
+}
 function App() {
 
   const [inputs,setInputs]=useState({
@@ -18,13 +23,13 @@ function App() {
   })
   const {username, email}=inputs
   //username과 email을 inputs에서 미리 추출
-  const onChange=e=>{
+  const onChange=useCallback(e=>{
     const {name,value}=e.target
     setInputs({
       ...inputs,
       [name]:value
     })
-  }
+  },[inputs]) //inputs가 바뀔 때에만 함수를 새로 만들어주고, 그렇지 않다면 기존의 함수를 재사용하게 됨
 
   const [users,setUsers]=useState([
     {
@@ -50,13 +55,13 @@ function App() {
   //굳이 리랜더링할 필요가 없기 때문에 변수를 기억하고 싶을 때 useRef를 사용할 수 있음
   //값이 바뀐다고 해서 컴포넌트가 리랜더링 되는 것음 아님!!!!!!
 
-  const onCreate=()=>{
+  const onCreate=useCallback(()=>{
     const user={
       id:nextId.current,
       username,
       email
     }
-    setUsers(users.concat(user));
+    setUsers(users=>users.concat(user));  //이 setUsers의 파라미터의 users에서 최신 파라미터를 조회하기 때문에 아래 deps에서 users를 작성해줄 필요가 없음
     // 위 아래 같은 코드 (spread로 복사하거나 concat으로 추가해서 이어주거나)
     // setUsers([...users,user]);  //기존 배열은 복사해서 넣으면서 user을 추가함
     // users.push로는 업데이트가 되지 않음!
@@ -68,18 +73,25 @@ function App() {
     console.log(nextId.current)
     nextId.current+=1
     //다음 값을 계속해서 1씩 업 시켜줌
-  }
+  },[username,email])    //의존하고 있는 값을 deps로 넣어주어야 업데이트 된 최신 값을 참조함(그렇지 않다면 기존 있는 값을 참조하기 때문)
 
-  const onRemove=id=>{
-    setUsers(users.filter(user=>user.id !==id))
-  }
+  const onRemove=useCallback(id=>{
+    setUsers(users=>users.filter(user=>user.id !==id))
+  },[]) //component가 처음 만들어질 때 딱 한 번 렌더링 된 후 그 다음부터는 계속 재사용됨
 
-  const onToggle=id=>{
-    setUsers(users.map(
+// ### 특정함수 재사용
+  const onToggle=useCallback(id=>{
+    setUsers(users=>users.map(
       user=>user.id===id?{...user,active: !user.active}:user
     ))
-  } //id 일치여부에 따라서만 업데이트(전체 업데이트가 아닌)
+  },[]) //id 일치여부에 따라서만 업데이트(전체 업데이트가 아닌)
     //기존의 user를 수정하는 것이 아니라 새로운 user를 만들어서 특정 값을 덮어씌워주는 형태로 구현
+    //222마찬가지
+
+
+// ### 연산된 값 재사용
+  const count=useMemo(()=>countActiveUsers(users),[users])
+    //deps([]안에 값)이 바뀔 때에만 활성화 됨(그 이외에는 이미 저장된 값 그대로 사용, 연산된 결과값을 재사용)
   return (
     <>
       <CreateUser
@@ -88,16 +100,9 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />  
-      <UserList3 users={users} onRemove={onRemove} onToggle={onToggle}/>    
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>    
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
-      //1. 태그는 모두 닫힌 태그여야 함
-      //2. 2개 이상의 태그는 모두 하나의 태그로 감싸줘야 함
-          //별도로 <></div>로 감싸면 불필요한 태그 없이 감쌀 수 있음
-      //3. ()는 가독성을 위해 사용한 것(없어도 괜찮음)
-      //4. 변수 사용 시(js 사용 시) {중괄호}로 감싼 후 사용
-      //5. react에서 style 사용 시 객체를 만든 후 사용해야 함
-      //6. css적용 시 css파일에서 작성 후 id를 이용해 적용
-      //7. JSX에서 주석 사용 시 {/* 주석 */}
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,64 +1,48 @@
-import React from 'react' 
+import React,{useEffect} from 'react' 
+// CreateUser 배열에 항목 삭제하기
 
-function User({user}){
+const User=React.memo(function User({user, onRemove, onToggle}){
+    const {username, email, id, active}=user
+    
+    useEffect(()=>{
+        console.log(user)   //특정 값이 업데이트 된 이후의 user정보를 보여줌
+        return ()=>{        //특정 값이 업데이트 되기 전의 user정보를 보여줌
+            console.log(user)  
+        }
+    },[user])   
     return (
         <div>
-            <b>{user.username}</b>
-            <span>({user.email})</span>
+            <b style={{
+                color: active?'green':'black',
+                cursor: 'pointer'
+                }}
+                onClick={()=>onToggle(id)}
+            >{username}</b>
+            &nbsp;
+            <span>({email})</span>
+            <button onClick={()=> onRemove(id)}>삭제</button>
         </div>
     )
-}
+})
 
-function UserList(){
-    const users=[
-        {
-            id: 1,
-            username:'a',
-            email:'first@example.com'
-        },
-        {
-            id: 2,
-            username:'b',
-            email:'second@example.com'
-        },
-        {
-            id: 3,
-            username:'c',
-            email:'third@example.com'
-        }
-    ]
+function UserList({users, onRemove, onToggle}){
     return(
         <div>
-            {   //배열을 랜더링 할 때는 key를 설정해줘야 효율적으로 사용할 수 있다!
+            {   
                 users.map(
-                    user=>(<User user={user} key={user.id}/>)
+                   ( user )=>(
+                   <User 
+                    user={user} 
+                    key={user.id} 
+                    onRemove={onRemove} 
+                    onToggle={onToggle}
+                    />
+                   )
                 )
             }
-
-            <User user={users[0]}/>
-            <User user={users[1]}/>
-            <User user={users[2]}/>
         </div>
     )
 }
-
-{/*
- return(
-        <div>
-            <div>
-                <b>{users[0].username}</b>
-                <span>({users[0].email})</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b>
-                <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b>
-                <span>({users[2].email})</span>
-            </div>
-        </div>
-    )
-
-*/}
-export default UserList
+//컴포넌트 최적화
+// ### 렌더링 된 결과물 재사용
+export default React.memo(UserList,(prevProps,nextProps)=>nextProps.users===prevProps.users)
